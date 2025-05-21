5700d411ec9af2fc4823da1b298861f81491617c
salominreji0621 <salominrejir@gmail.com>
2025-05-21 08:36:59 -0700

    Create right side element array .c

---
+++ right side element array .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("Result:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", sum - a[i]);
+    }
+    return 0;
+}
+
